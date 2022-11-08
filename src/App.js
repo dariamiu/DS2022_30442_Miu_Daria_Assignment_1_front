d83b059e09626375cc28933c3a0775f4f4e1129e
Daria Miu <miu.daria@gmail.com>
2022-11-08 15:04:30 +0200

    initial-commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import AdminAddClient from "./components/AdminAddClient";
+import AdminAddDevice from "./components/AdminAddDevice";
+import AdminInitialView from "./components/AdminInitialView";
+import AdminManageClients from "./components/AdminManageClients";
+import AdminManageDevices from "./components/AdminManageDevices";
+import LoginAdmin from "./components/LoginAdmin";
+import LoginClient from "./components/LoginClient";
+import StartPage from "./components/StartPage";
+import ClientInitialView from "./components/ClientInitialView";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path ="/" element={<StartPage />} />
+        <Route exact path="/client" element={<LoginClient/>}/>
+        <Route exact path="/admin" element={<LoginAdmin />}/>
+        <Route exact path="/admin/initial-view" element={<AdminInitialView />}/>
+        <Route exact path="/admin/add-client" element={<AdminAddClient/>}/>
+        <Route exact path="/admin/add-device" element={<AdminAddDevice/>}/>
+        <Route exact path="/admin/manage-devices" element={<AdminManageDevices/>}/>
+        <Route exact path="/admin/manage-clients" element={<AdminManageClients/>}/>
+        <Route exact path="/client/initial-view" element={<ClientInitialView/>}/>
+      </Routes>
+    </Router>
   );
 }
 
